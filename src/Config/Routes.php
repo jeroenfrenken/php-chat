a7dc8a651e014dedc42449b9fe6550016d918de3
Jeroen Frenken <jeroenfrenken@icloud.com>
2019-04-11 14:41:09 +0200

    🎉 Init

---
+++ src/Config/Routes.php
@@ -0,0 +1,9 @@
+<?php
+namespace App\Config;
+
+return [
+    [
+        'url' => '/chat',
+        'controller' => 'App\Controller\ChatController::getChats'
+    ]
+];
---
+++ src/Controller/ChatController.php
@@ -0,0 +1,15 @@
+<?php
+
+
+namespace App\Controller;
+
+
+class ChatController
+{
+
+    public function getChats()
+    {
+
+    }
+
+}
