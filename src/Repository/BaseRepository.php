617118c7e2cea64597676f69e5de03f64d669d03
Jeroen Frenken <jeroenfrenken@icloud.com>
2019-04-11 23:54:03 +0200

    🎨 Improved baserepository

--- src/Repository/BaseRepository.php
+++ src/Repository/BaseRepository.php
@@ -11,7 +11,14 @@ class BaseRepository
     public function __construct(array $config)
     {
         $this->pdo = new PDO("sqlite:{$config['url']}");
+        /*
+         * Set Charset to utf8
+         */
         $this->pdo->query('SET NAMES utf8');
+        /*
+         * Disable ATTR_EMULATE_PREPARES
+         * https://stackoverflow.com/questions/134099/are-pdo-prepared-statements-sufficient-to-prevent-sql-injection
+         */
         $this->pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }
 
